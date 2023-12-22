e5093b481f49a39e720a92d4e3eb8555a1d60a21
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2023-12-22 19:42:32 +0700

    Add files via upload

---
+++ Task2.2/Program (3).cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Kuzakov
+{
+    static void Main() {
+        int[] nums = new int[100];
+        int count = 1;
+        for (int i = 0; i < 100; i++)
+        {
+            nums[i] = count;
+            count += 2;
+        }
+        foreach(int i in nums)
+        {
+            Console.WriteLine($"{i} ");
+        }
+    }
+}
