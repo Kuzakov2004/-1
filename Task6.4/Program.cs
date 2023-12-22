1caea2d24ef8c89c5af6793ec8ecb7e0e6f253b1
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2023-12-22 12:43:26 +0700

    Add files via upload

---
+++ Task6.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Kuzakov {
+    static void Main()
+    {
+        int x = 0;
+        Console.WriteLine("Введите а");
+        int a = int.Parse(Console.ReadLine());
+        int sum = 0;
+        Console.WriteLine("Вводите числа");
+        for (;;)
+        {
+            x = int.Parse(Console.ReadLine());
+            if (x < 0)
+            {
+                break;
+            }
+
+            if (x % a == 0)
+            {
+                sum += x;
+            }
+        }
+        Console.WriteLine($"Сумма равна {sum}");
+    }
+}
