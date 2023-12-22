5b71514194c290be4192e2d2a2b15911630be236
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2023-12-22 20:08:26 +0700

    Add files via upload

---
+++ Task5.1/Program (5).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+class Kuzakov {
+    static void Main()
+    {
+        string path = "../../../numsTask1.txt";
+        string data = File.ReadAllText(path);
+        int[] nums = data.Split(' ').Select(int.Parse).ToArray();
+        int min = nums[0];
+        int minIndx = 0;
+        int prod = 1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (min > nums[i])
+            {
+                min = nums[i];
+                minIndx = i;
+            }
+        }
+
+        for (int i = minIndx + 1; i < nums.Length; i++)
+        {
+            prod *= nums[i];
+        }
+        Console.WriteLine(prod);
+    }
+}
