a5ff24fe2f2881d16d2abf84367fdb2cf20a6f46
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2023-12-22 19:33:44 +0700

    Update Program.cs

--- Task1.4/Program.cs
+++ Task1.4/Program.cs
@@ -7,7 +7,6 @@ class Kuzakov
             int start = int.Parse(Console.ReadLine());
             int end = int.Parse(Console.ReadLine());
             Random rnd = new Random();
-            string res = "";
             
             for(int i = 0; i < arr.Length; i++){
                 arr[i] = rnd.Next(start, end);
