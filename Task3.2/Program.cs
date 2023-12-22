b197f2bda296ee00554f61564e23bb14e4e77108
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2023-12-22 12:34:12 +0700

    Add files via upload

---
+++ Task3.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Kuzakov {
+    static void Main()
+    {
+        string path = "../../../nums.txt";
+        string rez = "";
+
+        string[] data = File.ReadAllLines(path);
+        
+        string[] nums = data[0].Split(' ').ToArray();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (int.Parse(nums[i]) % 2 != 0)
+            {
+                rez = rez + nums[i] + " ";
+            }
+        }
+        File.WriteAllText(path, rez);
+    }
+}
