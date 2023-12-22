8f8d57da84847d326fec103f70cb3ad43aaf3f2f
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2023-12-22 12:46:13 +0700

    Add files via upload

---
+++ Task6.5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Kuzakov {
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine()); 
+        int[,] arr = new int[n, m];
+        int[] count = new int[n];
+        int sum = 0;
+        Random rnd = new Random();
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                arr[i, j] = rnd.Next(0, 2); 
+                Console.Write($"{arr[i, j]} \t");
+                sum += arr[i, j];
+            }
+            
+            if (sum % 2 != 0)
+            {
+                count[i] = 1;
+            }
+            sum = 0;
+            
+            Console.WriteLine(count[i]);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Task6.6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Kuzakov {
+    static void Main()    {
+        Random rnd = new Random();        
+        double[] arr = new double[rnd.Next(5, 12)];
+        int countPos = 0;        
+        int countNeg = 0;
+        
+        for (int i = 0; i < arr.Length; i++)
+        {            
+            arr[i] = rnd.NextDouble() * ((10.0) - (-10.0)) + (-10.0);
+            arr[i] = Math.Round(arr[i], 2);            
+            Console.WriteLine(arr[i]);
+            
+            if (arr[i] < 0)            
+            {
+                countNeg++;            
+            }
+            else if (arr[i] > 0)            
+            {
+                countPos++;            
+            }
+        }        
+        double[] positive = new double[countPos];        
+        double[] negative = new double[countNeg];
+        countPos = 0;        
+        countNeg = 0;
+        
+        for (int i = 0; i < arr.Length; i++)
+        {            
+            if (arr[i] < 0)
+            {                
+                negative[countNeg] = arr[i];
+                countNeg++;            }
+            else if (arr[i] > 0)           
+            {
+                positive[countPos] = arr[i];                
+                countPos++;
+            }        
+        }
+        
+        Console.WriteLine("Отрицательные ");   
+        
+        foreach (double i in negative)
+        {            
+            Console.WriteLine(i);
+        }        
+        Console.WriteLine("Положительные ");
+        
+        foreach (double i in positive)        
+        {
+            Console.WriteLine(i);        
+        }
+    }
+}
