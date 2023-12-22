a91eea54ced959e84a09206693d5a68ae5b7654c
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2023-12-22 12:36:48 +0700

    Add files via upload

---
+++ Task4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Kuzakov{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());        
+        int prod = 1;
+            
+        for (int i = 3; i <= n; i += 3)
+        {        
+            prod *= i;
+            
+        }        
+        if (n < 3)        
+        {
+                Console.WriteLine("Число n меньше трёх");
+                
+        }
+        else        
+        {
+                Console.WriteLine(prod);
+                
+        }
+    
+    }
+}
---
+++ Task4.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Kuzakov {
+    static void Main()
+    {
+        string path = "../../../numsTask2.txt";
+        double sum = 0;
+        string data = File.ReadAllText(path);
+        double[] nums = data.Split(';').Select(double.Parse).ToArray();
+
+        foreach (double i in nums)
+        {
+            if (i == 0)
+            {
+                break;
+            }
+
+            if (i > 0)
+            {
+                sum += i;
+            }
+            
+        }
+        Console.WriteLine($"Сумма равна {sum}");
+    }
+}
