ae59b2a0c00772937a1c3406e5348a98b41f4fc0
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2023-12-22 12:24:56 +0700

    Add files via upload

---
+++ Task1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Kuzakov {
+    static void Main()
+    {
+        Random rnd = new Random();
+        int[] arr = new int[10];
+        int num;
+        int answer = 0;
+        
+        for (int i = 0; i < 10; i++)
+        {
+            arr[i] = rnd.Next();
+        }
+
+        
+        num = arr[0];
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (arr[i] < num)
+            {
+                num = arr[i];
+                answer = i;
+            }
+        }
+        Console.WriteLine($"Индекс минимального элемента {answer}");
+    }
+}
---
+++ Task1.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+class Kuzakov {
+    static void Main()
+    {
+        List<int> nums = new List<int>();
+        int inp, sum = 0, prod = 1, average;
+        for (;;)
+        {
+            inp = int.Parse(Console.ReadLine());
+            
+            if (inp == 0)
+            {
+                break;
+            }
+            nums.Add(inp);
+        }
+
+        foreach (int i in nums)
+        {
+            sum += i;
+            prod *= i;
+        }
+
+        average = sum / nums.Count;
+        
+        Console.WriteLine($"Сумма = {sum}");
+        Console.WriteLine($"Произведение = {prod}");
+        Console.WriteLine($"Среднее среди всех элементов = {average}");
+    }
+}
