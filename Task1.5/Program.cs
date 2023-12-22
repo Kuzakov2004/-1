28e75b696e33ff7649acdf6301788ebe4d844704
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2023-12-22 12:28:23 +0700

    Add files via upload

---
+++ Task1.5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Kuzakov
+{
+    static void Main() {
+        string str = Console.ReadLine();
+        int count = 0;
+        bool flag = true;
+       
+        for (int i = 0; i < str.Length; i++) 
+        { 
+            if (str[i] != ' ' && flag) 
+            { 
+                flag = false; 
+                count++; 
+            } 
+            else if (str[i] != ' ' && !flag) 
+            {
+                continue;
+            }
+            else 
+            {
+                flag = true;
+            }
+        }
+        Console.WriteLine("Start " + str + " End");
+        Console.WriteLine($"Количество слов в строке : {count}");
+        
+    }
+}
