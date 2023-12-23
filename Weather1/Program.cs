2227537ff25223b223788bb98456786a8f159f36
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2023-12-23 10:44:46 +0700

    Update Program.cs

--- Weather1/Program.cs
+++ Weather1/Program.cs
@@ -1,4 +1,3 @@
-﻿
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,31 +7,27 @@ using System.Text.Json.Serialization;
 
 internal class Program
 {
-    static string _path = @"C:\Users\dakuz\RiderProjects\Weather\Weather\City.txt";
-    
-    
+    static string _path = "../../../City.txt";
+
+
     public class Temper
     {
-        [JsonPropertyName("temp")]
-        public double Value { get; set; }    
+        [JsonPropertyName("temp")] public double Value { get; set; }
     }
+
     public class City
     {
-        [JsonPropertyName("id")]
-        public int Id { get; set; }           
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-        [JsonPropertyName("main")]
-        public Temper Temperature { get; set; }    
+        [JsonPropertyName("id")] public int Id { get; set; }
+        [JsonPropertyName("name")] public string Name { get; set; }
+        [JsonPropertyName("main")] public Temper Temperature { get; set; }
     }
-    
+
     public class Info
     {
-        [JsonPropertyName("list")]
-        public List<City> Cities{ get; set; }  
+        [JsonPropertyName("list")] public List<City> Cities { get; set; }
     }
-    
-    
+
+
     public static string GetJsonStringFromUrl(string url)
     {
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
@@ -47,38 +42,41 @@ internal class Program
         response.Close();
         return jsonString;
     }
-    
+
     static void Main()
     {
         string city = File.ReadAllText(_path);
         string responseBody;
         try
-        { 
-            responseBody = GetJsonStringFromUrl("http://openweathermap.org/data/2.5/find?q=" + city + "&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric");
+        {
+            responseBody = GetJsonStringFromUrl("http://openweathermap.org/data/2.5/find?q=" + city +
+                                                "&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric");
         }
         catch
         {
             Console.WriteLine("Города по умолчаию не обноруженно или город указан не верно");
         }
-       
+
         Info info = new Info();
 
         if (city != "")
         {
             responseBody = GetJsonStringFromUrl("http://openweathermap.org/data/2.5/find?q=" + city +
-                                                            "&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric");
-            info = JsonSerializer.Deserialize< Info >(responseBody);
+                                                "&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric");
+            info = JsonSerializer.Deserialize<Info>(responseBody);
             foreach (var c in info.Cities)
             {
-                Console.WriteLine("{0} - {1} C", c.Name, Math.Round(c.Temperature.Value-273.15, 2, MidpointRounding.ToEven));
+                Console.WriteLine("{0} - {1} C", c.Name,
+                    Math.Round(c.Temperature.Value - 273.15, 2, MidpointRounding.ToEven));
             }
         }
-        
+
         string choose;
         bool flag = true;
-        while(flag)
+        while (flag)
         {
-            Console.WriteLine("Выберите команду (города вводить на англиском языке с малненькой буквы): \n 1 - сохранить/изменить город по умолчанию \n 2 - узнать погоду по городу с клавиатуры \n 3 - завершить работу программы");
+            Console.WriteLine(
+                "Выберите команду (города вводить на англиском языке с малненькой буквы): \n 1 - сохранить/изменить город по умолчанию \n 2 - узнать погоду по городу с клавиатуры \n 3 - завершить работу программы");
             choose = Console.ReadLine();
             switch (choose)
             {
@@ -90,19 +88,22 @@ internal class Program
                 case "2":
                     Console.WriteLine("Введите город ");
                     city = Console.ReadLine();
-                    responseBody = GetJsonStringFromUrl("http://openweathermap.org/data/2.5/find?q=" + city + "&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric");
-        
-                    info = JsonSerializer.Deserialize< Info >(responseBody);
+                    responseBody = GetJsonStringFromUrl("http://openweathermap.org/data/2.5/find?q=" + city +
+                                                        "&appid=439d4b804bc8187953eb36d2a8c26a02&units=metric");
 
-                    if (info == null)
+                    info = JsonSerializer.Deserialize<Info>(responseBody);
+
+                    if (info.Cities.Count == 0)
                     {
                         Console.WriteLine("Не найдена информация по городу {0}", city);
-                        return;
                     }
+
                     foreach (var c in info.Cities)
                     {
-                        Console.WriteLine("{0} - {1} C", c.Name, Math.Round(c.Temperature.Value-273.15, 2, MidpointRounding.ToEven));
+                        Console.WriteLine("{0} - {1} C", c.Name,
+                            Math.Round(c.Temperature.Value - 273.15, 2, MidpointRounding.ToEven));
                     }
+
                     break;
                 case "3":
                     Console.WriteLine("Программа завершина ");
@@ -113,5 +114,6 @@ internal class Program
                     break;
             }
         }
-        
-    }   
+
+    }
+}
